9a11891a7262474fd628b7c01468634333d765e3
yoga1810 <yoalakshmi.c@gmail.com>
2026-02-28 13:18:49 +0530

    Fix background color and text color in CSS

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ st.markdown("""
 html, body, [class*="css"] {
     font-family: 'DM Sans', sans-serif;
     background-color: #080c14 ;
+    color : #080c14 ;
 }
 .main { background-color: #080c14; }
 .block-container { padding: 2rem 2.5rem; max-width: 1150px; }
